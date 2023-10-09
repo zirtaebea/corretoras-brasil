a269014fce4fad5363cbcb8bbf9ee02206042365
zirtaebea <zirtaeb.dev@gmail.com>
2023-10-08 21:38:52 -0300

    tratamento-bases-add-bd

--- functions.py
+++ functions.py
@@ -1,19 +1,213 @@
-import requests
-from plyer import notification
 
-# verificador de apis
+# * ----------------- IMPORTANDO BIBLIOTECAS
+import requests
+import pandas as pd
+from plyer import notification
+import sqlite3
+
+
+
+# ! ----------------- FUNÇÃO PARA VERIFICAR APIS
 def verificar_api(url, nome_api, lista):
     resp = requests.get(url)
+    # se a resposta for diferente de 200 (indisponivel)
     if resp.status_code !=200: 
+        # colocar nome da api na lista de apis indisponíveis
         lista.append(nome_api)
-        
-# notify de erro caso a api não esteja disponível
+
+
+
+# ! ----------------- FUNÇÃO NOTIFY API INDISPONÍVEL
 def api_indisponivel(lista):
+    # se a lista de apis indisponíveis for true, ou seja, se tiver algum item
     if lista: 
+        # notify de erro 
         message = f'Não foi possível acessar as seguintes APIs: {", ".join(lista)}'
     notification.notify(
         title='APIs Indisponíveis',
         message=message,
         timeout=15
     )
-    
+
+
+
+# ! ----------------- FUNÇÃO PARA RETORNAR MUNICIPIOS DA API DO IBGE
+def retorna_municipios(siglaUF):
+    # url da api de municipios do ibge
+    url = f'https://brasilapi.com.br/api/ibge/municipios/v1/{siglaUF}?providers=dados-abertos-br,gov,wikipedia'
+    resp = requests.get(url)
+    # se a api estiver disponível
+    if resp.status_code == 200:
+        # salve os dados json em data_municipios
+        data_municipios = resp.json()
+        # retorne o valor de data_municipios para ser armazenado na variavel do .py principal
+        return data_municipios
+    else:
+        # se não, imprima essa mensagem de erro
+        print(f"Falha na solicitação da API para {siglaUF}.")
+
+
+
+# ! ----------------- FUNÇÃO PARA TRATAR DADOS DA API MUNICIPIOS IBGE
+def trata_municipio(muni):
+    # lista para salvar os dados tratados dos municipios
+    lista_municipios = []
+    # para cada municipio por uf presente na lista de municipios brutos aninhados
+    for lista_municipios_uf in muni:
+        # para cada municipio puro presente na lista de municipio por uf 
+        for municipio in lista_municipios_uf:
+            # atribuindo colunas
+            nome = municipio['nome']
+            codigo_ibge = municipio['codigo_ibge']
+            uf = municipio['uf']
+            # adicionando dados tratados a lista de municipios
+            lista_municipios.append({'uf': uf,'nome': nome, 'codigo_ibge': codigo_ibge})
+    # retornando os dados tratados para serem armazenados na variavel do .py principal
+    return lista_municipios
+
+
+
+# ! ----------------- FUNÇÃO STRING EM MAIÚSCULO
+def texto_em_maiusculo(data, col):
+    # colocando caracteres em maiusculo
+    data[col] = data[col].str.upper()
+
+
+
+# ! -----------------  FUNÇÃO TRANSFORMAR STRING EM DATETIME
+def string_data(data):
+    # para cada cabeçalho de coluna presente na coluna de um determinado dataframe
+    for column in data.columns:
+        # se o cabeçalho da coluna conter 'data'
+        if 'data' in column:
+            # altere o tipo da coluna para datetime
+            data[column] = pd.to_datetime(data[column])
+
+
+
+# ! ----------------- FUNÇÃO PARA VERIFICAR SE EXISTEM CAMPOS VAZIOS (QUE NÃO PEGAM .isna())
+def string_vazia(data, coluna):
+    # filtrando colunas com '' 
+    resultado = (data[coluna] == '').sum()
+    # printando resultado
+    vazio = f"A coluna {coluna} do Dataframe possui {resultado} campos em branco"
+    return vazio
+
+
+
+# ! ----------------- FUNÇÃO TRANSFORMAR STRING '' EM INT
+def transforma_int(data, coluna):
+    # substituindo '' por 0
+    data[coluna] = data[coluna].replace('', 0)
+    # transformando para int
+    data[coluna] = data[coluna].astype(int)
+    # verificando
+    data.info()
+
+
+
+# ! ----------------- FUNÇÃO PARA ELIMINAR VALORES 0 DO TIPO INT
+def elimina_vazios_int(data, coluna):
+    # filtrando apenas por valores diferentes de zero
+    data = data[data[coluna] != 0]
+    # resetando indices
+    data.reset_index(drop=True, inplace=True)
+    # retornando valor para armazenar na variavel .py principal
+    return data
+
+
+
+# ! ----------------- FUNÇÃO PARA ELIMINAR VALORES DE TELEFONE COM MENOS DE 7 DIGITOS
+def elimina_tel(data, coluna):
+    # mudando a coluna para type string
+    data[coluna] = data[coluna].astype(str)
+    # filtro para selecionar registros com o número de caracteres igual ou maior que 7
+    data = data[data[coluna].str.len() >= 7]
+    # transformando para int
+    data.loc[:, coluna] = data[coluna].astype(int)
+    # resetando indice
+    data.reset_index(drop=True, inplace=True)
+    # retornando valor para armazenar na variavel .py principal
+    return data
+
+
+
+# ! -----------------  FUNÇÃO PARA ADICIONAR NÚMERO 3 EM TELEFONES COM 7 DIGITOS
+def adiciona_3_telefone(data, coluna):
+    # filtro transformando a coluna em string e selecionando apenas os telefones com 7 digitos
+    filtro = data[coluna].astype(str).str.len() == 7
+    # adicionando '3' apenas aos registros filtrados
+    data.loc[filtro, coluna] = '3' + data.loc[filtro, coluna].astype(str)
+    # convertendo a coluna de volta para inteiros 
+    data[coluna] = data[coluna].astype(int)
+    # retornando valor para armazenar na variavel .py principal
+    return data
+
+
+
+# ! -----------------  FUNÇÃO PARA TRANSFORMAR EM FLOAT
+def transforma_float(data, coluna):
+    # transformando valor para float
+    data[coluna] = data[coluna].astype(float)
+    # arredondando para duas casas decimais
+    data[coluna] = data[coluna].round(2)
+
+
+
+# ! ----------------- FUNÇÃO PARA REMOVER CARACTERES ESPECIAIS 
+def remove_caractere_especial(data, col):
+    # substituido carecteres especiais na string
+    data[col] = data[col].str.replace('Ç', 'C')
+    data[col] = data[col].str.replace('Á', 'A')
+    data[col] = data[col].str.replace('Ã', 'A')
+    data[col] = data[col].str.replace('À', 'A')
+    data[col] = data[col].str.replace('Â', 'A')
+    data[col] = data[col].str.replace('É', 'E')
+    data[col] = data[col].str.replace('È', 'E')
+    data[col] = data[col].str.replace('Ê', 'E')
+    data[col] = data[col].str.replace('Í', 'I')
+    data[col] = data[col].str.replace('Ì', 'I')
+    data[col] = data[col].str.replace('Ó', 'O')
+    data[col] = data[col].str.replace('Ò', 'O')
+    data[col] = data[col].str.replace('Ô', 'O')
+    data[col] = data[col].str.replace('Ú', 'U')
+    data[col] = data[col].str.replace('Ù', 'U')
+
+# ! ----------------- FUNÇÃO PARA VISUALIZAR TABELAS DO DB 
+def tabelas_bd():
+    # fazendo conexão
+    conn = sqlite3.connect('00_db/corretoras-brasil.db')
+    # definindo query
+    query = "SELECT name FROM sqlite_master WHERE type='table'"
+    # salvado os dados em um dataframe
+    schema = pd.read_sql(query, conn)
+    # imprimindo resultados
+    print(schema)
+    # fechando conexão
+    conn.close()
+
+
+
+# ! ----------------- FUNÇÃO PARA SALVAR TABELA NO DB
+def salva_bd(df, nome_tabela):
+    # fazendo conexão
+    conn = sqlite3.connect('00_db/corretoras-brasil.db')
+    # salvando dataframe na tabela
+    df.to_sql(nome_tabela, conn, if_exists='replace', index=False)
+    # fechando conexão
+    conn.close()
+
+
+
+# ! ----------------- FUNÇÃO PARA VISUALIZAR UMA TABELA EM ESPECIFICO NO DB
+def carrega_bd(nome_tabela):
+    # fazendo conexão
+    conn = sqlite3.connect('00_db/corretoras-brasil.db')
+    # fazendo a consulta da tabela nome_tabela
+    query = f"SELECT * FROM {nome_tabela}"
+    # salvando resultado em uma variavel
+    consulta = pd.read_sql(query, conn)
+    # fechando conexão
+    conn.close()
+    # retornando resultado obtido da consulta
+    return consulta
--- tables.py
+++ tables.py
@@ -1,34 +1,287 @@
+
+# * ----------------- IMPORTANDO BIBLIOTECAS
+
 import requests
 import functions as fun
 import pandas as pd
 
+# * ----------------- ACESSANDO APIS
+# ! 1) LISTA DE ERRO
 # lista para armazenar os nomes das APIs que não foram carregadas com sucesso
 api_erro = []
 
-# pegando dados da API
+
+
+# ! 2) URL DAS APIS
+# corretoras
 resp_corretoras = "https://brasilapi.com.br/api/cvm/corretoras/v1"
-resp_bancos = "https://brasilapi.com.br/api/banks/v1"
-resp_cidade = "https://brasilapi.com.br/api/cptec/v1/cidade"
+
+# estados
 resp_estados = "https://brasilapi.com.br/api/ibge/uf/v1"
 
-# verificação de apis usando a função verificar_api
+
+
+# ! 3) RETORNANDO DADOS DAS APIS
+
+# 3.1) pegando dados API municipios do IBGE
+# definindo lista de municipios para armazenar os dados da api de municípios
+dados_municipios = []
+
+# a api de municípios é necessita de uma chave para ser chamada (a sigla da unidade federativa)
+# lista de siglas das unidades federativas para inserir na url da api e gerar dados dos municipios
+siglas_uf = ['RO', 'AC', 'AM', 'RR', 'PA', 'AP', 'TO', 'MA', 'PI', 
+    'CE', 'RN', 'PB', 'PE', 'AL', 'SE', 'BA', 'MG', 
+    'ES', 'RJ', 'SP', 'PR', 'SC', 'RS', 'MS', 'MT', 'GO', 'DF']
+
+# iterador de siglas para poder acessar os dados de município de cada uf
+# para cada uf na lista siglas_uf
+for uf in siglas_uf:
+        # foi feita uma função cujo objetivo é retornar os dados da api
+        # armazenando os dados da função (que recebe a uf dentro da lista de siglas_uf)
+        muni_uf = fun.retorna_municipios(uf)
+        # para cada cidade presente na lista muni_uf
+        for cidade in muni_uf:
+            # criando coluna uf para adicionar a lista de dados da api
+            cidade['uf'] = uf
+            # fazendo um append de todos os municipios de cada uf na lista dados_municipio
+            dados_municipios.append(muni_uf)
+
+# tratando lista dados municipios para tirar dados aninhados
+data_municipios = fun.trata_municipio(dados_municipios)
+
+
+# 3.2) verificação de apis de corretoras e estados usando a função verificar_api
+#corretoras
 fun.verificar_api(resp_corretoras, "Corretoras", api_erro)
-fun.verificar_api(resp_bancos, "Bancos", api_erro)
-fun.verificar_api(resp_cidade, "Cidades", api_erro)
+
+#estados
 fun.verificar_api(resp_estados, "Estados", api_erro)
 
-# se api x estiver indisponível:
+# se uma das apis (corretoras ou estado) estiver indisponível:
 if api_erro: 
     fun.api_indisponivel(api_erro)
 # se não, armazene os dados nessas variáveis:    
 else:
     data_corretoras = requests.get(resp_corretoras).json()
-    data_bancos = requests.get(resp_bancos).json()
-    data_cidade = requests.get(resp_cidade).json()
     data_estados = requests.get(resp_estados).json()
 
-# colocando dados em um dataframe
+
+
+#  ! 4) COLOCANDO DADOS DAS APIS EM DATAFRAMES
+#corretoras
 corretoras = pd.DataFrame(data_corretoras)
-bancos = pd.DataFrame(data_bancos)
-cidade = pd.DataFrame(data_cidade)
+
+# estados
 estados = pd.DataFrame(data_estados)
+
+# municipios
+municipios = pd.DataFrame(data_municipios)
+
+
+
+
+# * ----------------- SALVANDO BASES BRUTAS .CSV
+# corretoras
+corretoras.to_csv('01_csv_files/dados_brutos/corretoras_bruto.csv')
+
+# estados
+estados.to_csv('01_csv_files/dados_brutos/estados_bruto.csv')
+
+# municipios
+municipios.to_csv('01_csv_files/dados_brutos/municipios_bruto.csv')
+
+
+
+
+# * ----------------- TRATAMENTO DE DADOS
+# ! 1) VERIFICAÇÃO DE DADOS
+# verificando as tabelas 
+
+# CORRETORAS
+# print(corretoras.info()) 
+
+# ESTADOS 
+# print(estados.info()) 
+
+# MUNICIPIOS
+# print(municipios.info()) 
+
+
+
+# ! 2) TRATANDO DADOS DF CORRETORAS
+# transformando dados de data de string para datetime
+fun.string_data(corretoras)
+
+# COLUNA 'telefone': 
+# eliminando linhas que não possuem números de telefone
+fun.string_vazia(corretoras, 'telefone') # * 41 campos em branco
+
+# transformando coluna para tipo int
+fun.transforma_int(corretoras, 'telefone')
+
+# eliminando vazios
+corretoras_tratado = fun.elimina_vazios_int(corretoras, 'telefone')
+
+# eliminando números com menos de 7 digitos
+corretoras_tratado = fun.elimina_tel(corretoras_tratado, 'telefone')
+
+# adicionando o número 3 em telefones que possuem 7 digitos
+corretoras_tratado = fun.adiciona_3_telefone(corretoras_tratado, 'telefone')
+
+
+# COLUNA 'email', 'complemento', 'bairro', 'type', 'pais', 'cep':
+# dropando colunas com muitos missing values não essenciais para a análise
+corretoras_tratado = corretoras_tratado.drop(['email', 'complemento', 'bairro', 'type', 'pais', 'cep'], axis=1)
+corretoras_tratado.reset_index(drop=True, inplace=True)
+
+
+# COLUNA 'cnpj': 
+# transformar para int
+fun.transforma_int(corretoras_tratado, 'cnpj')
+
+
+# COLUNA 'codigo_cvm': 
+# transformar para int
+fun.transforma_int(corretoras_tratado, 'codigo_cvm')
+
+
+# COLUNA 'valor_patrimonio_liquido', 'logradouro', 'municipio', 'nome_comercial':
+# remover valores vazios pontuais nas demais colunas que possuem valores vazios 
+fun.string_vazia(corretoras_tratado, 'valor_patrimonio_liquido') #* 3 em branco
+fun.string_vazia(corretoras_tratado, 'logradouro') #* 1 em branco
+fun.string_vazia(corretoras_tratado, 'municipio') #* 1 em branco -- se apagar o registro em branco do logradouro, apaga esse registro tb
+fun.string_vazia(corretoras_tratado, 'nome_comercial') #* 14 valores em branco
+
+# valor_patrimonio_liquido 
+corretoras_tratado = corretoras_tratado[corretoras_tratado['valor_patrimonio_liquido']!= '']
+
+# logradouro
+corretoras_tratado = corretoras_tratado[corretoras_tratado['logradouro']!= '']
+
+#nome_comercial
+corretoras_tratado = corretoras_tratado[corretoras_tratado['nome_comercial']!= '']
+corretoras_tratado = corretoras_tratado[corretoras_tratado['nome_comercial']!= '--']
+corretoras_tratado = corretoras_tratado[corretoras_tratado['nome_comercial']!= '-----']
+
+
+# COLUNA 'valor_patrimonio_liquido':
+# transformar para float com duas casas decimais
+fun.transforma_float(corretoras_tratado, 'valor_patrimonio_liquido')
+
+# remover valores = 0
+corretoras_tratado = corretoras_tratado[corretoras_tratado['valor_patrimonio_liquido'] != 0]
+
+# COLUNA 'nome_social', 'nome_comercial', 'logradouro':
+# strings em maiúsculo
+fun.texto_em_maiusculo(corretoras_tratado, 'nome_social')
+fun.texto_em_maiusculo(corretoras_tratado, 'nome_comercial')
+fun.texto_em_maiusculo(corretoras_tratado, 'logradouro')
+
+# COLUNA 'municipio':
+# retirar caracteres especiais
+fun.remove_caractere_especial(corretoras_tratado, 'municipio')
+
+# TODAS AS COLUNAS
+# ordenando colunas
+nova_ordem_colunas = ['codigo_cvm','cnpj', 'nome_social', 'nome_comercial', 'status', 'telefone', 'uf', 'municipio', 'logradouro', 'data_patrimonio_liquido', 'valor_patrimonio_liquido', 'data_inicio_situacao', 'data_registro']
+corretoras_tratado = corretoras_tratado[nova_ordem_colunas]
+
+
+# resetando index
+corretoras_tratado.reset_index(drop=True, inplace=True)
+
+
+
+# ! 3) TRATANDO DADOS DF ESTADOS
+
+# reorganizando colunas
+# COLUNA 'id'
+id = [estados['id'] for estados in data_estados]
+
+# COLUNA 'uf'
+sigla = [estados['sigla'] for estados in data_estados]
+
+# COLUNA 'nome'
+nome = [estados['nome'] for estados in data_estados]
+
+# COLUNA 'regiao'
+regiao = [estados['regiao']['nome'] for estados in data_estados]
+
+# COLUNA 'sigla_regiao'
+sigla_regiao = [estados['regiao']['sigla'] for estados in data_estados]
+
+# adicionando dados tratados em um novo dataframe
+estados_tratado = pd.DataFrame({
+    "uf": sigla, 
+    "id": id,
+    "nome": nome,
+    "regiao": regiao,
+    "sigla_regiao": sigla_regiao
+})
+
+# COLUNA 'nome' e 'regiao':
+# colocando caracteres em maiusculo
+fun.texto_em_maiusculo(estados_tratado, 'nome')
+fun.texto_em_maiusculo(estados_tratado, 'regiao')
+
+# tirando caracteres especiais
+fun.remove_caractere_especial(estados_tratado, 'nome')
+
+
+# COLUNA 'id':
+# transformando para int
+fun.transforma_int(estados_tratado, 'id')
+
+
+
+# ! 4) TRATANDO DADOS DF MUNICIPIOS
+# TODAS AS COLUNAS:
+# removendo duplicatas
+municipios.drop_duplicates()
+
+# resetando index
+municipios.reset_index(drop=True, inplace=True)
+
+# COLUNA 'nome':
+# colocando todos os nomes em maíusuculo
+fun.texto_em_maiusculo(municipios, 'nome')
+
+# retirando caracteres especiais
+fun.remove_caractere_especial(municipios, 'nome')
+
+# renomeando a coluna 'nome' para 'municipio'
+municipios_tradado = municipios.rename(columns={'nome': 'municipio'})
+
+# TODAS AS COLUNAS:
+# reordenando colunas
+nova_ordem_municipio = ['codigo_ibge', 'municipio', 'uf']
+municipios_tratado = municipios_tradado[nova_ordem_municipio]
+
+
+
+
+# * ----------------- SALVANDO BASES TRATADAS .CSV
+# corretoras
+corretoras_tratado.to_csv('01_csv_files/dados_tratados/corretoras_tratado.csv')
+
+# estados
+estados_tratado.to_csv('01_csv_files/dados_tratados/estados_tratado.csv')
+
+# municipios
+municipios_tratado.to_csv('01_csv_files/dados_tratados/municipios_tratado.csv')
+
+
+
+
+# * ----------------- ARMAZENANDO DADOS EM UM DB
+# corretoras
+fun.salva_bd(corretoras_tratado, 'corretoras')
+
+# estados
+fun.salva_bd(estados_tratado, 'estados')
+
+# municipios
+fun.salva_bd(municipios_tratado, 'municipios')
+
+# consultando
+fun.tabelas_bd()

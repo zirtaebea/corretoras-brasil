d3cb6d727217eb578007794763efe0c610bbb712
zirtaebea <zirtaeb.dev@gmail.com>
2023-11-14 21:46:13 -0300

    proj-final

--- README.md
+++ README.md
@@ -1,18 +1,6 @@
 # corretoras-brasil
 
-Projeto final do curso de Python da Coderhouse. Trata-se de um banco de dados sobre as corretoras de títulos e valores mobiliários presentes no sistema de Comissão de Valores Mobiliários (CVM), cujos dados foram adquiridos pela API do Brasil API.
-
-## 📌 Sobre
-
-O objetivo deste projeto é criar um banco de dados sobre as corretoras de títulos e valores mobiliários no país, sendo relacionados com os dados municipais e estaduais disponibilizados pelo Instituto Brasileiro de Geografia e Estatística - IBGE. Os dados foram obtidos pela Brasil API, que compila dados sobre diversas instâncias sobre o Brasil.
-
-### API
-
-Para coleta dos dados, foi utilizado a API do Brasil API, que conta com diversos dados do Brasil. A documentação da API utilizada pode ser acessada clicando [aqui](https://brasilapi.com.br/docs#tag/Corretoras). Para o projeto, foram utilizadas as seguintes APIs:
-
-- CVM
-- Munícipios (IBGE)
-- Unidades Federativas (IBGE)
+Projeto final do curso de Python da Coderhouse. O objetivo desta aplicação foi desenvolver um pipeline de dados, incluindo extração, tratamento e armazenamento dos dados em um Banco de Dados.
 
 ## 📋 Pré-requisitos
 
@@ -23,18 +11,52 @@ Para coleta dos dados, foi utilizado a API do Brasil API, que conta com diversos
 
 Baixe os arquivos:
 
-- [tables.py]()
-- [functions.py]()
+- [tables.py](https://github.com/zirtaebea/corretoras-brasil/blob/main/tables.py)
+- [functions.py](https://github.com/zirtaebea/corretoras-brasil/blob/main/functions.py)
 
-### Arquivos
+## 💻 APIs
 
-O arquivo **tables.py** possui o caminho de extração, tratamento e armazenamento dos dados em .csv e em um banco de dados (corretoras-brasil.db).
+Para a elaboração do projeto, foram consumidos os dados disponibilizados pela **Brasil API** que compila dados sobre diversas instâncias sobre o Brasil. Foram utilizadas as APIs:
 
-O arquivo **functions.py** possui as funções que serão implementadas dentro do arquivo principal de tabelas (tables.py) e funções de notificação em caso de erro de execução de alguma API.
+- Corretoras
+- Munícipios (IBGE)
+- Unidades Federativas (IBGE)
+
+### Corretoras
+
+Retorna as corretoras de títulos e valores mobiliários presentes nas informações da CVM.
+
+```
+https://brasilapi.com.br/api/cvm/corretoras/v1
+```
+
+### Municípios (IBGE)
+
+Retorna os municípios de uma determinada unidade federativa (uf).
+
+```
+https://brasilapi.com.br/api/ibge/municipios/v1/{siglaUF}?providers=dados-abertos-br,gov,wikipedia
+```
+
+Onde `siglaUf` refere-se a unidade federativa desejada.
+
+### Unidades federativas (IBGE)
+
+Retorna as unidades federativas do Brasil
+
+```
+https://brasilapi.com.br/api/ibge/uf/v1
+```
+
+## 📁 Arquivos
+
+O arquivo **[tables.py](https://github.com/zirtaebea/corretoras-brasil/blob/main/tables.py)** possui o caminho de extração, tratamento e armazenamento dos dados em .csv e em um banco de dados (corretoras-brasil.db).
+
+O arquivo **[functions.py](https://github.com/zirtaebea/corretoras-brasil/blob/main/functions.py)** possui as funções que serão implementadas dentro do arquivo principal de tabelas e funções de notificação em caso de erro de execução de alguma API.
 
 ### Importando bibliotecas
 
-Você pode conferir quais bibliotecas e suas respectivas versões utilizadas na elaboração do projeto acessando [requirements.txt]().
+Você pode conferir quais bibliotecas e suas respectivas versões utilizadas na elaboração do projeto acessando [requirements.txt](https://github.com/zirtaebea/corretoras-brasil/blob/main/requirements.txt).
 
 ## ⚙️ Estrutura e execução
 
@@ -43,9 +65,8 @@ O processo de recebimento dos dados, tratamento e armazenamento está dividido d
 1. Acessando dados das APIs
 2. Salvando dados brutos em .csv
 3. Tratamento de dados
-4. Stack e unstack para visualização de dados
-5. Salvando bases tratadas em .csv
-6. Armazenamento dos dados tratados em um banco de dados relacional
+4. Salvando bases tratadas em .csv
+5. Armazenamento dos dados tratados em um banco de dados relacional
 
 ## 🔩 Testes
 
@@ -124,147 +145,9 @@ municipios.head()
 
 Lembrando que o processo de verificação do tratamento desses dados é contínuo durante toda a execução a fim de manter os dados com maior qualidade possível.
 
-#### Funções de tratamento de dados
-
-O tratamento de dados foi otimizado com algumas funções. 
-
-A função ```trata_municipio()``` trata os dados iniciais recebidos pela API de municípios. A função recebe como parâmetro o DataFrame que contem os dados brutos dos municípios. Os dados em questão são estão aninhados, ou seja, trata-se de uma lista dentro de uma lista. O objetivo da função é justamente desaninhar os dados e deixá-los em formato de tabela.
-```
-def trata_municipio(muni):
-    # lista para salvar os dados tratados dos municipios
-    lista_municipios = []
-    # para cada municipio por uf presente na lista de municipios brutos aninhados
-    for lista_municipios_uf in muni:
-        # para cada municipio puro presente na lista de municipio por uf 
-        for municipio in lista_municipios_uf:
-            # atribuindo colunas
-            nome = municipio['nome']
-            codigo_ibge = municipio['codigo_ibge']
-            uf = municipio['uf']
-            # adicionando dados tratados a lista de municipios
-            lista_municipios.append({'uf': uf,'nome': nome, 'codigo_ibge': codigo_ibge})
-    # retornando os dados tratados para serem armazenados na variavel do .py principal
-    return lista_municipios
-``` 
-
-
-Para deixar os caracteres em maiúsculo, utilize ```texto_em_maiusculo()```, que recebe como parâmetros o DataFrame e a coluna que você deseja deixar em maiúsculo.
-```
-def texto_em_maiusculo(data, col):
-    # colocando caracteres em maiusculo
-    data[col] = data[col].str.upper()
-```
-
-
-A transformação de dados de string para datetime é feita por meio da função ```string_data()```, que recebe como parâmetro o DataFrame e identifica as colunas que possuem o nome 'data'. 
-```
-def string_data(data):
-    # para cada cabeçalho de coluna presente na coluna de um determinado dataframe
-    for column in data.columns:
-        # se o cabeçalho da coluna conter 'data'
-        if 'data' in column:
-            # altere o tipo da coluna para datetime
-            data[column] = pd.to_datetime(data[column])
-```
-
-
-
-Os campos que possuem valores vazios, mas que não são considerados nulos, são tratados com a função ```string_vazia()```, que recebe como parâmetros o DataFrame e a coluna que deseja tratar. 
-```
-def string_vazia(data, coluna):
-    # filtrando colunas com '' 
-    resultado = (data[coluna] == '').sum()
-    # printando resultado
-    vazio = f"A coluna {coluna} do Dataframe possui {resultado} campos em branco"
-    return vazio
-```
-
-
-
-Para eliminar campos vazios string em campos int utilize ```elimina_vazios_int()```, que recebe o DataFrame e a coluna e transforma os '' em zeros.
-```
-def elimina_vazios_int(data, coluna):
-    # filtrando apenas por valores diferentes de zero
-    data = data[data[coluna] != 0]
-    # resetando indices
-    data.reset_index(drop=True, inplace=True)
-    # retornando valor para armazenar na variavel .py principal
-    return data
-```
-
-A função ```transforma_float()```, recebe um DataFrame e uma coluna como parâmetros e tem como objetivo transformar campos em float com duas casas decimais. 
-```
-def transforma_float(data, coluna):
-    # transformando valor para float
-    data[coluna] = data[coluna].astype(float)
-    # arredondando para duas casas decimais
-    data[coluna] = data[coluna].round(2)
-```
-
-O tratamento de caracteres especiais é feito pela função ```remove_caractere_especial()```, recebendo o DF e a coluna que deseja ser tratada. 
-```
-def remove_caractere_especial(data, col):
-    # substituido carecteres especiais na string
-    data[col] = data[col].str.replace('Ç', 'C')
-    data[col] = data[col].str.replace('Á', 'A')
-    data[col] = data[col].str.replace('Ã', 'A')
-    data[col] = data[col].str.replace('À', 'A')
-    data[col] = data[col].str.replace('Â', 'A')
-    data[col] = data[col].str.replace('É', 'E')
-    data[col] = data[col].str.replace('È', 'E')
-    data[col] = data[col].str.replace('Ê', 'E')
-    data[col] = data[col].str.replace('Í', 'I')
-    data[col] = data[col].str.replace('Ì', 'I')
-    data[col] = data[col].str.replace('Ó', 'O')
-    data[col] = data[col].str.replace('Ò', 'O')
-    data[col] = data[col].str.replace('Ô', 'O')
-    data[col] = data[col].str.replace('Ú', 'U')
-    data[col] = data[col].str.replace('Ù', 'U')
-```
-
-O tratamento da coluna de telefone do DataFrame corretoras é feito com a função ```adciona_3_telefone()```, recebendo o DF e a coluna que deseja ser tratada. O objetivo da função é adicionar o número 3 em telefones que contenham apenas 7 dígitos. 
-```
-def adiciona_3_telefone(data, coluna):
-    # filtro transformando a coluna em string e selecionando apenas os telefones com 7 digitos
-    filtro = data[coluna].astype(str).str.len() == 7
-    # adicionando '3' apenas aos registros filtrados
-    data.loc[filtro, coluna] = '3' + data.loc[filtro, coluna].astype(str)
-    # convertendo a coluna de volta para inteiros 
-    data[coluna] = data[coluna].astype(int)
-    # retornando valor para armazenar na variavel .py principal
-    return data
-```
-
 ### Consulta de tabelas
 
 Para consultar as tabelas criadas você pode utilizar a seguinte função **tabelas_bd()**.
 
-```
-def tabelas_bd():
-    # fazendo conexão
-    conn = sqlite3.connect('00_db/corretoras-brasil.db')
-    # definindo query
-    query = "SELECT name FROM sqlite_master WHERE type='table'"
-    # salvado os dados em um dataframe
-    schema = pd.read_sql(query, conn)
-    # imprimindo resultados
-    print(schema)
-    # fechando conexão
-    conn.close()
-```
-
 Para consultar uma tabela em específico você pode utilizar a seguinte função **carrega_bd()**.
 
-```
-def carrega_bd(nome_tabela):
-    # fazendo conexão
-    conn = sqlite3.connect('00_db/corretoras-brasil.db')
-    # fazendo a consulta da tabela nome_tabela
-    query = f"SELECT * FROM {nome_tabela}"
-    # salvando resultado em uma variavel
-    consulta = pd.read_sql(query, conn)
-    # fechando conexão
-    conn.close()
-    # retornando resultado obtido da consulta
-    return consulta
-```
--- functions.py
+++ functions.py
@@ -1,26 +1,34 @@
 
-# * ----------------- IMPORTANDO BIBLIOTECAS
 import requests
 import pandas as pd
 from plyer import notification
 import sqlite3
 
-# """"""
 
-# ! ----------------- FUNÇÃO PARA VERIFICAR APIS
 def verificar_api(url, nome_api, lista):
+    """
+    Verifica APIs indisponíveis
+
+    Parâmetros:
+    url: url da API
+    nome_api: nome da API
+    lista: lista de APIs indisponíveis
+
+    """
     resp = requests.get(url)
-    # se a resposta for diferente de 200 (indisponivel)
     if resp.status_code != 200:
-        # colocar nome da api na lista de apis indisponíveis
         lista.append(nome_api)
 
 
-# ! ----------------- FUNÇÃO NOTIFY API INDISPONÍVEL
 def api_indisponivel(lista):
-    # se a lista de apis indisponíveis for true, ou seja, se tiver algum item
+    """
+    Notifica APIs indisponíveis
+
+    Parâmetros:
+    lista: lista de apis indisponíveis
+
+    """
     if lista:
-        # notify de erro
         message = f'Não foi possível acessar as seguintes APIs: {", ".join(lista)}'
     notification.notify(
         title='APIs Indisponíveis',
@@ -29,25 +37,38 @@ def api_indisponivel(lista):
     )
 
 
-# ! ----------------- FUNÇÃO PARA RETORNAR MUNICIPIOS DA API DO IBGE
 def retorna_municipios(siglaUF):
-    # url da api de municipios do ibge
+    """
+    Retorna os dados brutos dos municípios por unidade federativa
+
+    Parâmetros:
+    siglaUF: Sigla da unidade federativa (Exemplo: SP, BA, RJ)
+
+    Retorna: 
+    Lista de municípios por unidade federativa. 
+    Caso a api estiver indisponível, uma mensagem de erro irá aparecer. 
+
+    """
     url = f'https://brasilapi.com.br/api/ibge/municipios/v1/{siglaUF}?providers=dados-abertos-br,gov,wikipedia'
     resp = requests.get(url)
-    # se a api estiver disponível
     if resp.status_code == 200:
-        # salve os dados json em data_municipios
         data_municipios = resp.json()
-        # retorne o valor de data_municipios para ser armazenado na variavel do .py principal
         return data_municipios
     else:
-        # se não, imprima essa mensagem de erro
         print(f"Falha na solicitação da API para {siglaUF}.")
 
 
-# ! ----------------- FUNÇÃO PARA TRATAR DADOS DA API MUNICIPIOS IBGE
 def trata_municipio(muni):
-    # lista para salvar os dados tratados dos municipios
+    """
+    Trata os dados recebidos pela API de municípios, desaninhando e renomeando colunas.
+
+    Parâmetros:
+    muni: lista com os dados dos municípios
+
+    Retorna: 
+    Lista de municípios tratados
+
+    """
     lista_municipios = []
     # para cada municipio por uf presente na lista de municipios brutos aninhados
     for lista_municipios_uf in muni:
@@ -60,92 +81,140 @@ def trata_municipio(muni):
             # adicionando dados tratados a lista de municipios
             lista_municipios.append(
                 {'uf': uf, 'nome': nome, 'codigo_ibge': codigo_ibge})
-    # retornando os dados tratados para serem armazenados na variavel do .py principal
     return lista_municipios
 
 
-# ! ----------------- FUNÇÃO STRING EM MAIÚSCULO
 def texto_em_maiusculo(data, col):
-    # colocando caracteres em maiusculo
+    """
+    Transforma um conjunto de strings em maiúsculo
+
+    Parâmetros:
+    data: dataframe a ser trabalhado
+    col: coluna na qual os caracteres serão alterados
+
+    """
     data[col] = data[col].str.upper()
 
 
-# ! -----------------  FUNÇÃO TRANSFORMAR STRING EM DATETIME
 def string_data(data):
-    # para cada cabeçalho de coluna presente na coluna de um determinado dataframe
+    """
+    Transforma dados de data que estão no formato string para datetime
+
+    Parâmetros:
+    data: dataframe trabalhado
+
+    """
     for column in data.columns:
-        # se o cabeçalho da coluna conter 'data'
         if 'data' in column:
-            # altere o tipo da coluna para datetime
             data[column] = pd.to_datetime(data[column])
 
 
-# ! ----------------- FUNÇÃO PARA VERIFICAR SE EXISTEM CAMPOS VAZIOS (QUE NÃO PEGAM .isna())
 def string_vazia(data, coluna):
-    # filtrando colunas com ''
+    """
+    Identifica e quantifica as colunas que possuem valores vazios que não são reconhecidos pelo método .isna()
+
+    Parâmetros:
+    data: dataframe trabalhado
+    coluna: coluna a ser trabalhada
+
+    Retorna: 
+    A quantidade de campos vazios de uma determinada coluna.
+
+    """
     resultado = (data[coluna] == '').sum()
-    # printando resultado
     vazio = f"A coluna {coluna} do Dataframe possui {resultado} campos em branco"
     return vazio
 
 
-# ! ----------------- FUNÇÃO TRANSFORMAR STRING '' EM INT
 def transforma_int(data, coluna):
-    # substituindo '' por 0
+    """
+    Transforma campos string '' em zeros 
+
+    Parâmetros:
+    data: dataframe trabalhado
+    coluna: coluna a ser trabalhada
+
+    """
     data[coluna] = data[coluna].replace('', 0)
-    # transformando para int
     data[coluna] = data[coluna].astype(int)
-    # verificando
-    data.info()
 
 
-# ! ----------------- FUNÇÃO PARA ELIMINAR VALORES 0 DO TIPO INT
 def elimina_vazios_int(data, coluna):
-    # filtrando apenas por valores diferentes de zero
+    """
+    Elimina os campos que possuem o valor 0 de uma determinada coluna
+
+    Parâmetros:
+    data: dataframe trabalhado
+    coluna: coluna a ser trabalhada
+
+    Retorna:
+    Dataframe filtrado sem os zeros na coluna indicada
+
+    """
     data = data[data[coluna] != 0]
-    # resetando indices
     data.reset_index(drop=True, inplace=True)
-    # retornando valor para armazenar na variavel .py principal
     return data
 
 
-# ! ----------------- FUNÇÃO PARA ELIMINAR VALORES DE TELEFONE COM MENOS DE 7 DIGITOS
 def elimina_tel(data, coluna):
-    # mudando a coluna para type string
+    """
+    Elimina campos que possuem telefones com menos de 7 dígitos
+
+    Parâmetros:
+    data: dataframe trabalhado
+    coluna: coluna a ser trabalhada
+
+    Retorna:
+    Dataframe filtrado apenas com telefones com 7 ou 8 dígitos 
+
+    """
     data[coluna] = data[coluna].astype(str)
-    # filtro para selecionar registros com o número de caracteres igual ou maior que 7
     data = data[data[coluna].str.len() >= 7]
-    # transformando para int
     data.loc[:, coluna] = data[coluna].astype(int)
-    # resetando indice
     data.reset_index(drop=True, inplace=True)
-    # retornando valor para armazenar na variavel .py principal
     return data
 
 
-# ! -----------------  FUNÇÃO PARA ADICIONAR NÚMERO 3 EM TELEFONES COM 7 DIGITOS
 def adiciona_3_telefone(data, coluna):
-    # filtro transformando a coluna em string e selecionando apenas os telefones com 7 digitos
+    """
+    Adiciona o número 3 na frente dos telefones que possuem 7 dígitos
+
+    Parâmetros:
+    data: dataframe trabalhado
+    coluna: coluna a ser trabalhada
+
+    Retorna:
+    Dataframe filtrado com todos os telefones com 8 dígitos
+
+    """
     filtro = data[coluna].astype(str).str.len() == 7
-    # adicionando '3' apenas aos registros filtrados
     data.loc[filtro, coluna] = '3' + data.loc[filtro, coluna].astype(str)
-    # convertendo a coluna de volta para inteiros
     data[coluna] = data[coluna].astype(int)
-    # retornando valor para armazenar na variavel .py principal
     return data
 
 
-# ! -----------------  FUNÇÃO PARA TRANSFORMAR EM FLOAT
 def transforma_float(data, coluna):
-    # transformando valor para float
+    """
+    Transforma campos em float de uma determinada coluna
+
+    Parâmetros:
+    data: dataframe trabalhado
+    coluna: coluna a ser trabalhada
+
+    """
     data[coluna] = data[coluna].astype(float)
-    # arredondando para duas casas decimais
     data[coluna] = data[coluna].round(2)
 
 
-# ! ----------------- FUNÇÃO PARA REMOVER CARACTERES ESPECIAIS
 def remove_caractere_especial(data, col):
-    # substituido carecteres especiais na string
+    """
+    Remove os caracteres especiais de uma coluna
+
+    Parâmetros:
+    data: dataframe trabalhado
+    col: coluna a ser trabalhada
+
+    """
     data[col] = data[col].str.replace('Ç', 'C')
     data[col] = data[col].str.replace('Á', 'A')
     data[col] = data[col].str.replace('Ã', 'A')
@@ -163,70 +232,102 @@ def remove_caractere_especial(data, col):
     data[col] = data[col].str.replace('Ù', 'U')
 
 
-# ! ----------------- FUNÇÃO PARA VISUALIZAR TABELAS DO DB
 def tabelas_bd():
-    # fazendo conexão
+    """
+    Estabelece uma conexão com o banco de dados e mostra todas as tabelas existentes nele.
+
+    """
     conn = sqlite3.connect('00_db/corretoras-brasil.db')
-    # definindo query
     query = "SELECT name FROM sqlite_master WHERE type='table'"
-    # salvado os dados em um dataframe
     schema = pd.read_sql(query, conn)
-    # imprimindo resultados
     print(schema)
-    # fechando conexão
     conn.close()
 
 
-# ! ----------------- FUNÇÃO PARA SALVAR TABELA NO DB
 def salva_bd(df, nome_tabela):
-    # fazendo conexão
+    """
+    Salva uma tabela nova no banco de dados
+
+    Parâmetros:
+    df: dataframe trabalhado
+    nome_tabela: nome dado a tabela dentro do banco de dados
+
+    """
     conn = sqlite3.connect('00_db/corretoras-brasil.db')
-    # salvando dataframe na tabela
     df.to_sql(nome_tabela, conn, if_exists='replace', index=False)
-    # fechando conexão
     conn.close()
 
 
-# ! ----------------- FUNÇÃO PARA VISUALIZAR UMA TABELA EM ESPECIFICO NO DB
 def carrega_bd(nome_tabela):
-    # fazendo conexão
+    """
+    Consulta uma tabela em um banco de dados
+
+    Parâmetros:
+    nome_tabela: nome da tabela no banco de dados
+
+    Retorna:
+    Dados da tabela obtidos durante a consulta
+
+    """
     conn = sqlite3.connect('00_db/corretoras-brasil.db')
-    # fazendo a consulta da tabela nome_tabela
     query = f"SELECT * FROM {nome_tabela}"
-    # salvando resultado em uma variavel
     consulta = pd.read_sql(query, conn)
-    # fechando conexão
     conn.close()
-    # retornando resultado obtido da consulta
     return consulta
 
 
-# ! ----------------- FUNÇÃO PARA REALIZAR UMA FUNÇÃO DE AGREGAÇÃO DE UMA COLUNA POR OUTRA
 def unstacked_count_agg(df, index, coluna, agg):
+    """
+    Realiza unstack (desempilhamento) de um dataframe
+
+    Parâmetros:
+    df: dataframe trabalhado
+    index: coluna a ser desempilhada
+    coluna: coluna para agregação
+    agg: o tipo de agregação
+
+    Retorna:
+    Dataframe desempilhado e agregado com base em outra coluna
+
+    """
     # agrupando o df e adicionando uma função de agregação
     grouped = df.groupby([f'{index}', f'{coluna}'])[f'{coluna}'].agg(agg)
     # unstack na segunda coluna
     unstacked = grouped.unstack(level=1)
-    # preenchendo os vazios com zero
     unstacked = unstacked.fillna(0)
-    # retornando resultado para ser armazenado em uma variável
     return unstacked
 
 
-# ! ----------------- FUNÇÃO PARA REALIZAR EMPILHAMENTO DE DADOS DE DETEMINADAS COLUNAS
 def stacked_tabela(df, index, colunas):
+    """
+    Realiza stack (empilhamento) de um dataframe
+
+    Parâmetros:
+    df: dataframe trabalhado
+    index: coluna para ser utilizada como index 
+    colunas: colunas a serem empuilhadas
+
+    Retorna:
+    Dataframe empilhado com base em determinadas colunas
+
+    """
     # df com as colunas a serem empilhadas
     stacked_df = df.set_index([index])[colunas].stack().reset_index()
-    # renomeando colunas
     stacked_df.columns = [index, 'colunas_empilhadas', 'valores']
-    # retornando resultado para ser armazenado em uma variável
     return stacked_df
 
 
-# ! ----------------- FUNÇÃO PARA REALIZAR EMPILHAMENTO DE DADOS DE DETEMINADAS COLUNAS
 def alerta_etapa_concluida(base, nome, etapa):
+    """
+    Alerta de etapa concluida
+
+    Parâmetros:
+    base: dataframe trabalhado
+    nome: nome do dataframe
+    etapa: etapa concluida
+
+    """
     if base:
-        # notify de erro
         message = f'A base {nome} foi {etapa} com sucesso'
         notification.notify(
             title='Concluído',
